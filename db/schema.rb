95fdcc94e4d82f22a1cc9602e7a7aeadf5735ca5
Nadav <nadav.matalon@gmail.com>
2014-07-01 14:22:21 +0100

    minor changes to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140701111709) do
+ActiveRecord::Schema.define(version: 20140701131016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: true do |t|
     t.string   "title"
-    t.string   "author"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
