4343b4576441628a8547b06b8d557e1a90bf832c
Nadav <nadav.matalon@gmail.com>
2014-07-01 17:11:40 +0100

    minor changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,40 @@
 class BooksController < ApplicationController
+
+	def index
+    	@books = Book.all
+	end
+
+	def new
+		@book = Book.new
+	end
+
+	def create
+		Book.create book_params
+		redirect_to '/books'
+	end
+
+	def edit
+		@book = Book.find params[:id]	
+	end
+
+	def update
+		@book = Book.find params[:id]
+		@book.update book_params
+		redirect_to '/books'
+	end
+
+	def destroy
+		@book = Book.find params[:id]
+		@book.destroy
+		flash[:notice] = "Successfully deleted #{@book.name}"
+		redirect_to '/books'
+	end
+
+	private
+
+	def book_params
+		params[:book].permit(:title)
+	end
 end
+
+
--- spec/features/books_feature_spec.rb
+++ spec/features/books_feature_spec.rb
@@ -1,27 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'book listing page' do
-  context 'no books' do
-    it 'tells me there a no books' do
-      visit '/books'
-      expect(page).to have_content 'No books yet'
-    end
-  end
+describe "book listing page" do
 
-  context 'are books' do
-    before do
-      Book.create(title: 'Snowcrash', author: 'Stephenson')
+    context "no books" do
+
+        it "tells me there a no books" do
+            visit "/books"
+            expect(page).to have_content "No books yet"
+        end
     end
 
-    it 'should show the title' do
-      visit '/books'
-      expect(page).to have_content 'Snowcrash'
+    context "there are books" do
+
+        before do
+            Book.create(title: "Snowcrash")
+        end
+
+        it "should show the title" do
+            visit "/books"
+            expect(page).to have_content "Snowcrash"
+        end
     end
 
-    it 'should show the author' do
-      visit '/books'
-      expect(page).to have_content 'Stephenson'
-    end
-
-  end
 end
