523ddaaf3febcb2321825b21754884b2ef3e3b07
Nadav <nadav.matalon@gmail.com>
2014-07-02 21:38:21 +0100

    added html and styling to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
  
+ root 'home#index'
+ 
  resources :books, :authors
 
+ 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
